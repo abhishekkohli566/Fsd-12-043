6c18cbe53ef4b59b475e2231de13f1631f91f974
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-07-23 11:53:53 +0530

    initial commit

---
+++ lab1/hello.js
@@ -0,0 +1,16 @@
+console.log("abhishek kohli");
+// write a function to take two Number and return
+//thier sum
+
+function sum(a ,b){
+    return a+b;
+}
+
+const add = (a ,b)=> {
+    return a + b;
+
+};
+const add2 = (a,b) => a+b;
+console.log(sum(20,10));
+console.log(add(20,1));
+console.log(add2(2,10));
