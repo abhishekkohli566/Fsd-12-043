1b72735bf072e61ea22f0b18cbd6e89134c55b20
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-07-30 11:57:33 +0530

    task commti

--- lab1/dom5.js
+++ lab1/dom5.js
@@ -11,9 +11,21 @@ class DomClass extends EventEmitter{
     dispatchEvent(eventName,eventData= {}){
         const event =  {
             type:eventName,
-            timespam =new Date(),
+            timespam:new Date(),
             ...eventData
         };
         this.emit(eventName,event);
         }
 }
+const button = new DomClass();
+const handleClick =(event) =>{
+    console.log(`Button clicked types: ${event.type} at ${event.timespam}`);
+};
+button.addEventListener("click",handleClick);
+button.dispatchEvent('click',{
+target: "submitBtn",
+});
+ button.removeEventListener("click",handleClick);
+ button.dispatchEvent('click',{
+target: "resetBtn",
+});
