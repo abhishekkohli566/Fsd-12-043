797aac35921a431bcb6951aa1e6b4005c7b36762
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-07-31 14:01:28 +0530

    test commit

--- lab2/fun.js
+++ lab2/fun.js
@@ -9,9 +9,13 @@ const f3 = ()=>{
 };
 
 function main (){
-  console.log("main")
-  f1();
-  f2();
+  console.log("main");
+
+  setTimeout(f1, 1000);
+  //setInterval(f1,1000);
+  setImmediate(f2);
+  
   f3();
   console.log("end")
 }
+main();
---
+++ lab2/readme.md
@@ -0,0 +1,9 @@
+# EventLoop
+
+ js is synchronous and single threaded bydefault 
+  
+  ## there can be async behaviour
+   - with browser\api -
+   setTimeout, setInterval,setImmediate, nextTick
+   - with promises
+   - with event haandlers
