dd55d40aa67cdec87ba9c4970d4cd0c49df60474
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-08-20 10:54:52 +0530

    task commit

--- lab2/file1.js
+++ lab2/file1.js
@@ -1,4 +1,11 @@
-import { writeFile, appendFile } from "fs/promises";
+// import { writeFile,appendFile } from "fs/promises";
+import { writeFile, appendFile,readFile } from "fs/promises";
 
-//await writeFile("hello.txt", "js is easy");
-await appendFile("hello.txt","\nFS is much easy than others")
+// await writeFile("hello.txt","Hello JS");
+await appendFile("hello.txt","\n JS is much easy than othe r. ❤️❤️")
+// asynchronous type of file 
+await appendFile("hello.txt", "\naayush kumar ");
+const content=await readFile("hello.txt","utf-8")
+console.log(content);
+
+
