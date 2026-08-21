6c54073548451056f31dd0e6e0be1c6c3bcf98ba
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-08-21 15:37:43 +0530

    prg2.js

--- lab3/prg2.js
+++ lab3/prg2.js
@@ -2,8 +2,8 @@ import http from 'http';
 
 const server = http.createServer(req, res =>{
     console.log("server hit by client");
-    req.write(<h1>Hello client</h1>);
-    res.end();
+   // req.write("<h1>Hello client</h1>"");
+    res.end("<h1>Hello client</h1>");
 });
 
 server.listen(4444, () =>  console.log("Server is running ..."));
