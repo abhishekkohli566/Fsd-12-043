f340302eac452a26c34477132d4bbbe268c11fcd
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-08-27 11:45:03 +0530

    prg4

--- lab3/prg4.js
+++ lab3/prg4.js
@@ -3,11 +3,20 @@ import http from 'http';
  const server = http.createServer((req,res) => {
      res.writeHead(200,{'conttent-type': 'apllication/json'});
 
-     const product ={
+     const product =[{
         name: "Iphone",
+        price: 85000,
         qty:2,
         discount: 15,
-     };
+     },
+     {
+      name: "laptop",
+      price: 155000,
+      qty:1,
+      discount: 25,
+     },
+    ];
+
      res.end(JSON.stringify(product));
  });
 
